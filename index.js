dc8b877fe6e211a53de7aa2c48e1522646a52cf0
Michael Kastner <kastner@galt.de>
2019-03-25 08:06:03 +0100

    added querystring support

--- index.js
+++ index.js
@@ -1,21 +1,68 @@
-module.exports = {
-  req: {
-    params: {}
-  },
-  res: {
-    status(val) {
-      this._status = val;
-      return this;
+const qs = require('qs');
+const fs = require('fs');
+const log = require('mk-log');
+
+module.exports = function MkAdapterTestHelpers() {
+
+  const req = {};
+  let querystring = '';
+
+  Object.defineProperties(req, {
+    body: {
+      value: {},
+      configurable: true,
+      enumerable: true,
+      writable: true 
     },
-    viewName: '',
-    data: {
+    params: {
+      value: {},
+      configurable: true,
+      enumerable: true
     },
-    render(view, viewData) {
-      let viewName = view;
-      let data = viewData;
-    },
-    json(jsonObj) {
-      this.data = jsonObj; 
+    query: {
+      set(newQuerystring) {
+        querystring = newQuerystring; 
+      },
+      get() {
+        return qs.parse(querystring); 
+      },
+      enumerable: true,
+      configurable: true
     }
-  }
+  }); 
+
+  return {
+    req,
+    res: {
+      setHeader(headerKey, headerValue) {
+        this.header[headerKey] = headerValue; 
+      },
+      header: {},
+      status(val) {
+        this._status = val;
+        return this;
+      },
+      viewName: '',
+      data: {
+      },
+      render(view, viewData) {
+        let viewName = view;
+        let data = viewData;
+      },
+      json(jsonObj) {
+        this.data = jsonObj; 
+      },
+      send(str) {
+        this._send = str; 
+      },
+      sendFile(filePath) {
+        let data = fs.readFileSync(filePath);
+        // Encode to base64
+        let encodedImage = new Buffer(data, 'binary').toString('base64'); 
+        // Just to keep in mind how to decode: decode to base64 
+        // let decodedImage = new Buffer(encodedImage, 'base64').toString('binary'); 
+        return encodedImage;     
+      }
+    }
+  };
 };
---
+++ test/index.test.js
@@ -0,0 +1,23 @@
+const tape = require('tape');
+const AdapterTestHelpers = require('../index.js');
+
+tape('querystring', (t) => {
+
+ 
+  let adapterTestHelpers = AdapterTestHelpers();
+  adapterTestHelpers.req.query = 'a[]=1&a[]=2'; 
+
+
+  t.ok(adapterTestHelpers.req.query.a, 'query parameter a exists');
+  t.equals(adapterTestHelpers.req.query.a.length, 2, 'query parameter has two elements');
+  adapterTestHelpers.req.params.id = '3'; 
+  t.equals(adapterTestHelpers.req.params.id, '3', 'query parameter a exists');
+
+  adapterTestHelpers.req.body = {a:'1'};
+  t.equals(adapterTestHelpers.req.body.a, '1', 'body property exists');
+
+
+
+  t.end();
+
+});
